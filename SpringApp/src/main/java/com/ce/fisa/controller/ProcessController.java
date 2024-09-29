380285a9c9c902a0290c0d844dc476142bbb3af4
bigkhk <85100718+bigkhk@users.noreply.github.com>
2024-09-29 18:43:55 +0900

    Update ProcessController.java

--- SpringApp/src/main/java/com/ce/fisa/controller/ProcessController.java
+++ SpringApp/src/main/java/com/ce/fisa/controller/ProcessController.java
@@ -11,7 +11,7 @@ public class ProcessController {
 	
 	@GetMapping("/test")
 	public String reqRes() {
-		log.info("요청 수락 ~~~");
-		return "수정 1 : linux 서버에서 실행되는 app";
+		log.info("요청 수락 2~~~");
+		return "수정 2: linux 서버에서 실행되는 app <br>";
 	}
 }
